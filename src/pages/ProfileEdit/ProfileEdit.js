79f19e9740c86ccb6b8b7c0127641539fab9b533
victor-hf <v.herrero.fernandez@gmail.com>
2022-11-19 18:25:57 +0100

    feature:profileEdit -work in progress

--- src/pages/ProfileEdit/ProfileEdit.js
+++ src/pages/ProfileEdit/ProfileEdit.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// Components
+import {
+    StyledAvatar,
+    StyledProfileCardEdit,
+    StyledUserDetailsContainer,
+    StyledTopContainer,
+    StyledContactSection,
+    StyledHobbiesSection,
+    StyledStatsContainer,
+    StyledHeader,
+    StyledItem,
+    StyledStat,
+} from './styles';
+
+import {
+    SecondaryButton,
+    PrimaryButton,
+} from '../../components/styledComponents/StyledButtons';
+
+import Jenniffer from '../../assets/images/users/jennifer.png';
 
 export const ProfileEdit = () => {
-    return <div>ProfileEdit</div>;
+    // const navigate = useNavigate();
+    // const toEditProfile = '/profile/edit';
+
+    return (
+        <StyledProfileCardEdit>
+            <StyledAvatar>
+                <div id="update-image">
+                    <img src={Jenniffer} alt="profile picture" />
+                    <SecondaryButton onClick={() => {}}>
+                        UPDATE IMAGE
+                    </SecondaryButton>
+                </div>
+                <div id="delete-save">
+                    <SecondaryButton onClick={() => {}}>
+                        DELETE ACCOUNT
+                    </SecondaryButton>
+                    <PrimaryButton onClick={() => {}}>
+                        UPDATE IMAGE
+                    </PrimaryButton>
+                </div>
+            </StyledAvatar>
+            <StyledUserDetailsContainer>FORM</StyledUserDetailsContainer>
+        </StyledProfileCardEdit>
+    );
 };
