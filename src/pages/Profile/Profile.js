084e79131d67e03eea106d741833871fc74e7003
victor-hf <v.herrero.fernandez@gmail.com>
2022-11-20 19:51:52 +0100

    refactor: profile now dynamic

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,8 +1,9 @@
 // Libraries
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getCurrentUser } from '../../store/slices/currentUser';
+import { selectUserToken } from '../../store/slices/loginUser';
 
 // Components
 import {
@@ -30,18 +31,75 @@ import Jenniffer from '../../assets/images/users/jennifer.png';
 export const Profile = () => {
     const navigate = useNavigate();
     const toEditProfile = '/profile/edit';
+
+    const currentUser = useSelector((state) => state.currentUser);
+    const token = useSelector(selectUserToken);
     const dispatch = useDispatch();
+
+    // State
+    // const [userStatus, setUserStatus] = React.useState(currentUser.status);
+    const [id, setId] = React.useState('');
+    const [firstName, setFirstName] = React.useState('');
+    const [lastName, setLastName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [job, setJob] = React.useState('');
+    const [location, setLocation] = React.useState('');
+    // const [phone, setPhone] = React.useState(currentUser.phone);
+    const [about, setAbout] = React.useState('');
+    const [avatar, setAvatar] = React.useState('');
+    const [banner, setBanner] = React.useState('');
+    const [thingsUserLikes, setThingsUserLikes] = React.useState(
+        currentUser.things_user_likes
+    );
+    const [amountOfPosts, setAmountOfPosts] = React.useState('');
+    const [amountOfLikes, setAmountOfLikes] = React.useState('');
+    const [amountOfFriends, setAmountOfFriends] = React.useState('');
+    const [amountOfFollowers, setAmountOfFollowers] = React.useState('');
+    const [amountFollowing, setAmountFollowing] = React.useState('');
+
+    // Methods
+    const onLoad = () => {
+        // console.log('body at load', body);
+        const payload = { token: `Bearer ${token}` };
+        dispatch(getCurrentUser(payload));
+    };
+
+    // Component did mount
+    useEffect(() => {
+        onLoad();
+    }, []);
+
+    // When store changes
+    useEffect(() => {
+        setId(currentUser.id);
+        setFirstName(currentUser.first_name);
+        setLastName(currentUser.last_name);
+        setEmail(currentUser.email);
+        setUsername(currentUser.username);
+        setLocation(currentUser.location);
+        setAbout(currentUser.about_me);
+        setAvatar(currentUser.avatar);
+        setBanner(currentUser.banner);
+        setThingsUserLikes(currentUser.things_user_likes);
+        setAmountOfPosts(currentUser.amount_of_posts);
+        setAmountOfLikes(currentUser.amount_of_likes);
+        setAmountOfFriends(currentUser.amount_of_friends);
+        setAmountOfFollowers(currentUser.amount_of_followers);
+        setAmountFollowing(currentUser.amount_following);
+    }, [currentUser]);
 
     return (
         <StyledProfileCard>
             <StyledAvatar>
-                <img src={Jenniffer} alt="profile picture" />
-                <span id="name">Jennifer Smith</span>
-                <span id="location">Zurich, Switzerland</span>
+                <img src={avatar} alt="profile picture" />
+                <span id="name">
+                    {(firstName ? firstName : '') +
+                        (lastName ? ' ' + lastName : '')}
+                </span>
+                <span id="location">{location}</span>
                 <SecondaryButton
                     onClick={() => {
-                        // console.log('dispatch');
-                        // dispatch(getCurrentUser('payload'));
                         navigate(toEditProfile);
                     }}
                 >
@@ -53,16 +111,12 @@ export const Profile = () => {
                     <StyledContactSection>
                         <div id="about">
                             <StyledHeader>About</StyledHeader>
-                            <p>
-                                Lorem ipsum dolor sit amet. Lorem ipsum dolor
-                                sit amet consectetur adipisicing elit. Atque,
-                                delectus!
-                            </p>
+                            <p>{about}</p>
                         </div>
                         <div id="contact-info">
                             <StyledItem>
                                 <StyledHeader>Email</StyledHeader>
-                                <p>jennifersmith@gmail.com</p>
+                                <p>{email}</p>
                             </StyledItem>
                             <StyledItem>
                                 <StyledHeader>Phone</StyledHeader>
@@ -74,36 +128,32 @@ export const Profile = () => {
                         <div className="preferences">
                             <StyledHeader>Things I like</StyledHeader>
                             <div className="list">
-                                <p>Cooking</p>
-                                <p>Travel</p>
-                                <p>Reading</p>
-                                <p>Swimming</p>
-                                <p>Dancing</p>
-                                <p>Hiking</p>
-                                <p>Knitting</p>
+                                {thingsUserLikes.map((hobby) => {
+                                    <p>{Object.values(hobby)}</p>;
+                                })}
                             </div>
                         </div>
                     </StyledHobbiesSection>
                 </StyledTopContainer>
                 <StyledStatsContainer>
                     <StyledStat id="posts">
-                        <p>34</p>
+                        <p>{amountOfPosts}</p>
                         <span>Posts</span>
                     </StyledStat>
                     <StyledStat id="likes">
-                        <p>256</p>
+                        <p>{amountOfLikes}</p>
                         <span>Likes</span>
                     </StyledStat>
                     <StyledStat id="friends">
-                        <p>98</p>
+                        <p>{amountOfFriends}</p>
                         <span>Friends</span>
                     </StyledStat>
                     <StyledStat id="followers">
-                        <p>129</p>
+                        <p>{amountOfFollowers}</p>
                         <span>Followers</span>
                     </StyledStat>
                     <StyledStat id="following">
-                        <p>154</p>
+                        <p>{amountFollowing}</p>
                         <span>Following</span>
                     </StyledStat>
                 </StyledStatsContainer>
