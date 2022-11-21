b2524e3d347d0caf4607d928bd4be35ec38bf38a
victor-hf <v.herrero.fernandez@gmail.com>
2022-11-21 21:38:01 +0100

    feat: upload avatar image - files can be uploaded using standard file input - profile edit styling (shorter input length) - profile styling (removed phone) - console log cleanup

--- src/components/styledComponents/StyledInput.js
+++ src/components/styledComponents/StyledInput.js
@@ -17,8 +17,8 @@ export const StyledInputText = (props) => {
 };
 
 export const InputText = styled.div`
-    min-width: 300px;
-    margin: 20px 45px;
+    min-width: 250px;
+    margin: 20px 30px;
 
     > * {
         width: 100%;
@@ -63,3 +63,61 @@ export const StyledInputHobbies = (props) => {
 };
 
 export const InputHobbies = styled.div``;
+
+export const StyledInputFile = (props) => {
+    return (
+        <InputFile>
+            <label htmlFor="fileUpload">{props.label}</label>
+            <input
+                name="fileUpload"
+                type="file"
+                // placeholder={props.placeholder}
+                onChange={props.onChange}
+                // value={props.value}
+            >
+                {props.innerHTML}
+            </input>
+        </InputFile>
+    );
+};
+
+export const InputFile = styled.div`
+    min-width: 250px;
+    margin: 20px 30px;
+
+    > * {
+        width: 100%;
+        display: block;
+    }
+
+    label {
+        font-size: 12px;
+        line-height: 30px;
+
+        color: #00000070;
+    }
+
+    input {
+        border: none;
+        border-bottom: 1px solid #00000020;
+    }
+
+    /* input {
+        display: none;
+    }
+
+    label {
+        display: none;
+        height: 40px;
+        width: 120px;
+        font-size: 10px;
+        border-radius: 30px;
+        border: 1px solid grey;
+        background-color: white;
+        text-transform: uppercase;
+
+        :hover {
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        }
+    } */
+`;
