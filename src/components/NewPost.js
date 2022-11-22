43ba271a77742990e37d1f225af3ad5585ea32bf
Alessio <alessiotortora@gmail.com>
2022-11-22 21:38:12 +0100

    style changes posts

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,46 +1,56 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUserToken } from '../store/slices/loginUser';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUserToken } from "../store/slices/loginUser";
 // import { addPost } from '../store/slices/posts';
-import { addNewPost } from '../store/slices/posts';
+import { addNewPost } from "../store/slices/posts";
+import { ProfileIcon } from "./Navbar/styles";
+import { PostSection, ProfilePost } from "./styledPosts/styles";
+import ProfilePic from "../assets/images/users/jennifer.png";
+import SendBtn from '../assets/svgs/send_button.svg';
 
 function NewPost() {
-    const [newPost, setNewPost] = useState('');
-    const dispatch = useDispatch();
-    const body = { newPost: newPost, token: useSelector(selectUserToken) };
+  const [newPost, setNewPost] = useState("");
+  const dispatch = useDispatch();
+  const body = { newPost: newPost, token: useSelector(selectUserToken) };
 
-    const handleAddPost = (e) => {
-        e.preventDefault();
-        // creates the dispatch action into a variable for easy reading
-        // const action = addPost(newPost);
-        // dispatch(addPost(newPost));
-        dispatch(addNewPost(body));
+  const handleAddPost = (e) => {
+    e.preventDefault();
+    // creates the dispatch action into a variable for easy reading
+    // const action = addPost(newPost);
+    // dispatch(addPost(newPost));
+    dispatch(addNewPost(body));
 
-        // dispatch(addNewPost({ title, content, user: userId })).unwrap()
+    // dispatch(addNewPost({ title, content, user: userId })).unwrap()
 
-        // cleanes the local state
-        setNewPost('');
-    };
+    // cleanes the local state
+    setNewPost("");
+  };
 
-    const handleNewPostChange = (e) => {
-        const newPost = e.currentTarget.value;
-        // sets the state with the input
-        setNewPost(newPost);
-    };
+  const handleNewPostChange = (e) => {
+    const newPost = e.currentTarget.value;
+    // sets the state with the input
+    setNewPost(newPost);
+  };
 
-    return (
-        <div className="NewPost">
-            <form onSubmit={handleAddPost}>
-                <input
-                    type="text"
-                    placeholder="Your next post"
-                    value={newPost}
-                    onChange={handleNewPostChange}
-                />
-                <input type="submit" value={'Add'} />
-            </form>
-        </div>
-    );
+  return (
+    <div className="NewPost">
+      <PostSection onSubmit={handleAddPost}>
+          <ProfilePost>
+            <img src={ProfilePic} alt="icon-profile" />
+          </ProfilePost>
+        <input
+          type="text"
+          placeholder="What's on your mind, name?"
+          value={newPost}
+          onChange={handleNewPostChange}
+        />
+        <button type="submit">
+            <img src={SendBtn} alt='send button'>
+            </img>
+        </button>
+      </PostSection>
+    </div>
+  );
 }
 
 export { NewPost };
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { NewPost } from './NewPost';
-import { fetchPosts } from '../store/slices/posts';
-import { selectUserToken } from '../store/slices/loginUser';
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NewPost } from "./NewPost";
+import { fetchPosts } from "../store/slices/posts";
+import { selectUserToken } from "../store/slices/loginUser";
+import { MenuPost, Post } from "./styledPosts/styles";
+import { ProfilePost } from "./styledPosts/styles";
+import ProfilePic from "../assets/images/users/jennifer.png";
+import MenuIcon from "../assets/svgs/menu.svg";
 // export const ReactionButton = ({ post }) => {
 //   const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
 //     return (
@@ -14,71 +17,85 @@ import { selectUserToken } from '../store/slices/loginUser';
 //   })
 
 const PostExcerpt = ({ post }) => {
-    return (
-        <article className="post-excerpt">
-            <h3>Post</h3>
-            <h3>{post.id}</h3>
-            <div>
-                {post.user.id}
-                {post.created}
-                {/* {post.logged_in_user_liked}    */}
-                <input
-                    type="checkbox"
-                    defaultChecked={post.logged_in_user_liked}
-                />
-                {/* <PostAuthor userId={post.user} /> */}
-                {/* <TimeAgo timestamp={post.created} /> */}
-            </div>
-            <p className="post-content">{post.content.substring(0, 100)}</p>
-            {/* <ReactionButtons post={post} /> */}
-            {/* <Link to={`/posts/${post.id}`} className="button muted-button">
+  return (
+    <article className="post-excerpt">
+      <h3>TEST</h3>
+      <h3>{post.id}</h3>
+      <div>
+        {post.user.id}
+        {post.created}
+        {/* {post.logged_in_user_liked}    */}
+        <input type="checkbox" defaultChecked={post.logged_in_user_liked} />
+        {/* <PostAuthor userId={post.user} /> */}
+        {/* <TimeAgo timestamp={post.created} /> */}
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      {/* <ReactionButtons post={post} /> */}
+      {/* <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link> */}
-        </article>
-    );
+    </article>
+  );
 };
 
 export function PostsList() {
-    const dispatch = useDispatch();
-    const posts = useSelector((state) => state.posts.posts);
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.posts);
 
-    const postStatus = useSelector((state) => state.posts.status);
-    const error = useSelector((state) => state.posts.error);
-    const payload = { token: useSelector(selectUserToken) };
+  const postStatus = useSelector((state) => state.posts.status);
+  const error = useSelector((state) => state.posts.error);
+  const payload = { token: useSelector(selectUserToken) };
 
-    useEffect(() => {
-        if (postStatus === 'idle') {
-            dispatch(fetchPosts(payload));
-        }
-    }, [postStatus, dispatch]);
-
-    let content;
-
-    if (postStatus === 'succeeded') {
-        console.log('success');
-        console.log(posts);
-        // Sort posts in reverse chronological order by created timestamp
-        const orderedPosts = posts
-            .slice()
-            .sort((a, b) => b.created.localeCompare(a.created));
-
-        console.log(orderedPosts);
-        content = orderedPosts.map((post) => (
-            <PostExcerpt key={post.id} post={post} />
-        ));
-    } else if (postStatus === 'failed') {
-        console.log('fail');
-        content = <div>{error}</div>;
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts(payload));
     }
+  }, [postStatus, dispatch]);
 
-    return (
-        <section className="posts-list">
-            <h2>Posts</h2>
-            {postStatus}
-            {content}
-            <NewPost />
-        </section>
-    );
+  let content;
+
+  if (postStatus === "succeeded") {
+    console.log("success");
+    console.log(posts);
+    // Sort posts in reverse chronological order by created timestamp
+     const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.created.localeCompare(a.created));
+
+    console.log(orderedPosts);
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    console.log("fail");
+    content = <div>{error}</div>;
+  }
+
+  console.log(content)
+
+  return (
+    <section className="posts-list">
+      <NewPost />
+
+      <Post>
+        <div className="top">
+          <ProfilePost>
+            <img src={ProfilePic} alt="icon-profile" />
+          </ProfilePost>
+
+          <div className="name">
+            <p>Jennifer Smith</p>
+            <p>Just now</p>
+          </div>
+
+          <MenuPost>
+            <img src={MenuIcon} alt="icon-profile" />
+          </MenuPost>
+        </div>
+        {content}
+      </Post>
+    </section>
+  );
 }
 
 export default PostsList;
