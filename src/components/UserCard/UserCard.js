b8757734d36e305bcb0f6bf4bd138a7e9382fe46
Zahoor Novman <zahoor.novman@gmail.com>
2022-11-22 23:14:53 +0100

    console log clean up. removing hard coded friends

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,65 +1,63 @@
-
 //Styled Components
-import { StyledUserCard, StyledDescription, StyledName } from "./StyledUserCard";
-import { StyledHobbiesIcon, StyledHobbies } from '../styledComponents/StyledHobbies';
-import { SecondaryButton } from '../styledComponents/StyledButtons';
+import {
+  StyledUserCard,
+  StyledDescription,
+  StyledName,
+} from "./StyledUserCard";
+import {
+  StyledHobbiesIcon,
+  StyledHobbies,
+} from "../styledComponents/StyledHobbies";
+import { SecondaryButton } from "../styledComponents/StyledButtons";
 
+function UserCard(props) {
+  let pprops = {
+    obj: {
+      id: 809,
+      email: "juans@propulsionacademy.com",
+      first_name: "Juan Miguel",
+      last_name: "Sánchez Arce",
+      username: "JuanMiguelSanchezArce",
+      job: "",
+      avatar:
+        "https://motion.propulsion-home.ch/media-files/JMSA_WhiteBackground.png",
+      banner: "https://motion.propulsion-home.ch/media-files/Groot_Revenge.jpg",
+      location: "Zurich, Switzerland",
+      about_me:
+        "Lorem ipsum dolor sit amet, vim ut quas volumus probatus, has tantas laudem iracundia et, ad per utamur ceteros apeirian",
+      things_user_likes: ["Cooking", "Travel", "Reading", "Swimming", "Gaming"],
+      logged_in_user_is_following: false,
+      logged_in_user_is_friends: false,
+      logged_in_user_is_rejected: false,
+      logged_in_user_received_fr: false,
+      logged_in_user_sent_fr: false,
+      amount_of_posts: 0,
+      amount_of_likes: 0,
+      amount_of_friends: 2,
+      amount_of_followers: 7,
+      amount_following: 0,
+    },
+  };
 
-function UserCard(props){
+  const fullName = props.obj.first_name + " " + props.obj.last_name;
 
-    let pprops = {
-    'obj':
-    {
-        "id": 809,
-        "email": "juans@propulsionacademy.com",
-        "first_name": "Juan Miguel",
-        "last_name": "Sánchez Arce",
-        "username": "JuanMiguelSanchezArce",
-        "job": "",
-        "avatar": "https://motion.propulsion-home.ch/media-files/JMSA_WhiteBackground.png",
-        "banner": "https://motion.propulsion-home.ch/media-files/Groot_Revenge.jpg",
-        "location": "Zurich, Switzerland",
-        "about_me": "Lorem ipsum dolor sit amet, vim ut quas volumus probatus, has tantas laudem iracundia et, ad per utamur ceteros apeirian",
-        "things_user_likes": ['Cooking', 'Travel', 'Reading', 'Swimming', 'Gaming'],
-        "logged_in_user_is_following": false,
-        "logged_in_user_is_friends": false,
-        "logged_in_user_is_rejected": false,
-        "logged_in_user_received_fr": false,
-        "logged_in_user_sent_fr": false,
-        "amount_of_posts": 0,
-        "amount_of_likes": 0,
-        "amount_of_friends": 2,
-        "amount_of_followers": 7,
-        "amount_following": 0
-    }
+  return (
+    <StyledUserCard>
+      <img src={props.obj.avatar} alt="Avatar" />
+      <StyledName>{fullName}</StyledName>
+      <StyledDescription>{props.obj.location}</StyledDescription>
+      <div>
+        <SecondaryButton>FOLLOW</SecondaryButton>
+        <SecondaryButton>ADD FRIEND</SecondaryButton>
+      </div>
+      <StyledDescription>{props.obj.about_me}</StyledDescription>
+      <StyledHobbies>
+        {props.obj.things_user_likes.map((item) => (
+          <StyledHobbiesIcon>{item}</StyledHobbiesIcon>
+        ))}
+      </StyledHobbies>
+    </StyledUserCard>
+  );
 }
 
- const fullName = pprops.obj.first_name + ' ' + pprops.obj.last_name;
-
-    return (
-        <StyledUserCard>
-                <img src={pprops.obj.avatar} alt='Avatar'/>
-                <StyledName>
-                    {fullName}
-                </StyledName>
-                <StyledDescription>
-                    {pprops.obj.location}
-                </StyledDescription>
-                <div>
-                    <SecondaryButton>FOLLOW</SecondaryButton>
-                    <SecondaryButton>ADD FRIEND</SecondaryButton>
-                </div>
-                <StyledDescription>
-                    {pprops.obj.about_me}
-                </StyledDescription>
-                <StyledHobbies>            
-                        {pprops.obj.things_user_likes.map( item =>
-                            <StyledHobbiesIcon>{item}</StyledHobbiesIcon>
-                        )}                   
-                </StyledHobbies>
-        </StyledUserCard>
-    )
-}
-
-
-export { UserCard }
+export { UserCard };
--- src/pages/FindFriends.js
+++ src/pages/FindFriends.js
@@ -1,11 +1,11 @@
 //hooks
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
 
 //styled components
 import { StyledFindFriendsPage } from "../components/styledComponents/StyledFindFriendsPage";
 
+//selectors
 import { selectUserToken } from "../store/slices/loginUser";
 
 //components
@@ -13,12 +13,6 @@ import { UserCard } from "../components/UserCard/UserCard";
 
 function FindFriends() {
   let token = useSelector(selectUserToken);
-  console.log(token);
-  if (token === "") {
-    token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY4ODcyOTU1LCJqdGkiOiIyMWM2NGZmNDA1YTk0MDllYjk0N2ZjYTAyYzI1ODAxNiIsInVzZXJfaWQiOjE5NzN9.NiwT2veTN1-uEoU8B1GaxW_41lMRREc1JsLmYickftI";
-    console.log("Hard Coded token used");
-  }
 
   const [list, setList] = useState([]);
   const [error, setError] = useState("");
@@ -35,15 +29,13 @@ function FindFriends() {
     };
 
     fetch(
-      "https://motion.propulsion-home.ch/backend/api/users/?limit=10&offset=0",
+      "https://motion.propulsion-home.ch/backend/api/users/?limit=6&offset=0",
       requestOptions
     )
       .then((response) => response.json())
       .then((result) => setList(result.results))
       .catch((error) => setError(error));
   }, []);
-
-  console.log(list);
 
   return (
     <StyledFindFriendsPage>
