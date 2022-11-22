1044ec6ff2bf7422ca63995cedf9fb7899b6701e
victor-hf <v.herrero.fernandez@gmail.com>
2022-11-22 19:41:17 +0100

    refactor: made token dynamic for posts

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,44 +1,46 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUserToken } from '../store/slices/loginUser';
 // import { addPost } from '../store/slices/posts';
-import { addNewPost } from "../store/slices/posts";
+import { addNewPost } from '../store/slices/posts';
 
 function NewPost() {
-  const [newPost, setNewPost] = useState("");
-  const dispatch = useDispatch();
+    const [newPost, setNewPost] = useState('');
+    const dispatch = useDispatch();
+    const body = { newPost: newPost, token: useSelector(selectUserToken) };
 
-  const handleAddPost = (e) => {
-    e.preventDefault();
-    // creates the dispatch action into a variable for easy reading
-    // const action = addPost(newPost);
-    // dispatch(addPost(newPost));
-    dispatch(addNewPost(newPost));
+    const handleAddPost = (e) => {
+        e.preventDefault();
+        // creates the dispatch action into a variable for easy reading
+        // const action = addPost(newPost);
+        // dispatch(addPost(newPost));
+        dispatch(addNewPost(body));
 
-    // dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        // dispatch(addNewPost({ title, content, user: userId })).unwrap()
 
-    // cleanes the local state
-    setNewPost("");
-  };
+        // cleanes the local state
+        setNewPost('');
+    };
 
-  const handleNewPostChange = (e) => {
-    const newPost = e.currentTarget.value;
-    // sets the state with the input
-    setNewPost(newPost);
-  };
+    const handleNewPostChange = (e) => {
+        const newPost = e.currentTarget.value;
+        // sets the state with the input
+        setNewPost(newPost);
+    };
 
-  return (
-    <div className="NewPost">
-      <form onSubmit={handleAddPost}>
-        <input
-          type="text"
-          placeholder="Your next post"
-          value={newPost}
-          onChange={handleNewPostChange}
-        />
-        <input type="submit" value={"Add"} />
-      </form>
-    </div>
-  );
+    return (
+        <div className="NewPost">
+            <form onSubmit={handleAddPost}>
+                <input
+                    type="text"
+                    placeholder="Your next post"
+                    value={newPost}
+                    onChange={handleNewPostChange}
+                />
+                <input type="submit" value={'Add'} />
+            </form>
+        </div>
+    );
 }
 
 export { NewPost };
--- src/store/slices/posts.js
+++ src/store/slices/posts.js
@@ -1,88 +1,88 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { selectUserToken } from './loginUser';
 
 export const fetchPosts = createAsyncThunk(
-  "user/getPosts}",
-  async (payload) => {
-    console.log(payload);
-    try {
-      const { data } = await axios.get(
-        "https://motion.propulsion-home.ch/backend/api/social/posts/me/",
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY5MzEwODEwLCJqdGkiOiJmNjdkMzg2YTYyOTA0MGI5ODBiNGFhYjBlZmU0MjdhOCIsInVzZXJfaWQiOjE5OTF9.3hcDgJKxCPs_QCaDQS__bFJIWg68B3nXKQwAjVUZ3Go",
-          },
-        },
-        payload
-      );
+    'user/getPosts}',
+    async (payload) => {
+        console.log(payload);
+        try {
+            const { data } = await axios.get(
+                'https://motion.propulsion-home.ch/backend/api/social/posts/me/',
+                {
+                    headers: {
+                        Authorization: `Bearer ${payload.token}`,
+                    },
+                },
+                payload
+            );
 
-      return data.results.map((post) => post);
-    } catch (err) {
-      console.log(err);
+            return data.results.map((post) => post);
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
 );
 
 export const addNewPost = createAsyncThunk(
-  "posts/addNewPost",
-  async (payload) => {
-    console.log(payload);
-    let raw = JSON.stringify({
-      content: `${payload}`,
-    });
-    console.log(payload);
+    'posts/addNewPost',
+    async (payload) => {
+        console.log(payload);
+        let raw = JSON.stringify({
+            content: `${payload.newPost}`,
+        });
+        console.log(payload.newPost);
 
-    try {
-      const { data } = await axios.post(
-        "https://motion.propulsion-home.ch/backend/api/social/posts/",
-        raw,
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY5MTE1NzI4LCJqdGkiOiI2YjJiMGYwNjllZTA0NGJiYTdlNzIzNjBlZGQ5Yjk5MCIsInVzZXJfaWQiOjE5OTF9.y0MGJbv6n6cSr2cQ70VWFMA73PuScw6pMoviJ-J_PI8",
-            "Content-Type": "application/json",
-          },
-        },
-        payload
-      );
+        try {
+            const { data } = await axios.post(
+                'https://motion.propulsion-home.ch/backend/api/social/posts/',
+                raw,
+                {
+                    headers: {
+                        Authorization: `Bearer ${payload.token}`,
+                        'Content-Type': 'application/json',
+                    },
+                },
+                payload
+            );
 
-      return data;
-    } catch (err) {
-      console.log(err);
+            return data;
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
 );
 
 const initialState = {
-  posts: [],
-  status: "idle",
-  error: null,
+    posts: [],
+    status: 'idle',
+    error: null,
 };
 
 const postsSlice = createSlice({
-  name: "posts",
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder
-      .addCase(fetchPosts.pending, (state, action) => {
-        state.status = "loading";
-      })
-      .addCase(fetchPosts.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        // Add any fetched posts to the array
-        state.posts = state.posts.concat(action.payload);
-      })
-      .addCase(fetchPosts.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      })
-      .addCase(addNewPost.fulfilled, (state, action) => {
-        // We can directly add the new post object to our posts array
-        state.posts.push(action.payload);
-      });
-  },
+    name: 'posts',
+    initialState,
+    reducers: {},
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                // Add any fetched posts to the array
+                state.posts = state.posts.concat(action.payload);
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                // We can directly add the new post object to our posts array
+                state.posts.push(action.payload);
+            });
+    },
 });
 
 // export const { addPost } = postsSlice.actions;
