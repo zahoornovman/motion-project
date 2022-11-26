ad0160f9884698687353ee45b1b2b83f16fd5ef8
victor-hf <v.herrero.fernandez@gmail.com>
2022-11-26 11:24:45 +0100

    feature: linkedStats in profile filter by current user - zindex fix for profile, banner and friends notification - overall gray background color

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,143 +1,152 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { NewPost } from "./NewPost";
-import { fetchPosts } from "../store/slices/posts";
-import { selectUserToken } from "../store/slices/loginUser";
-import { MenuPost, Post } from "./styledPosts/styles";
-import { ProfilePost } from "./styledPosts/styles";
-import { PostsWrap } from "./styledPosts/styles";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NewPost } from './NewPost';
+import { fetchPosts } from '../store/slices/posts';
+import { selectUserToken } from '../store/slices/loginUser';
+import { MenuPost, Post } from './styledPosts/styles';
+import { ProfilePost } from './styledPosts/styles';
+import { PostsWrap } from './styledPosts/styles';
 
-import ProfilePic from "../assets/images/users/jennifer.png";
-import MenuIcon from "../assets/svgs/menu.svg";
-import Share from "../assets/svgs/share.svg";
-import Heart from "../assets/svgs/heart.svg";
+import ProfilePic from '../assets/images/users/jennifer.png';
+import MenuIcon from '../assets/svgs/menu.svg';
+import Share from '../assets/svgs/share.svg';
+import Heart from '../assets/svgs/heart.svg';
 
-import { format, formatDistance, subDays } from "date-fns";
+import { format, formatDistance, subDays } from 'date-fns';
 
 const PostExcerpt = ({ post }) => {
-  return (
-    <>
-      <Post>
-        <div className="top">
-          <ProfilePost>
-            <img src={ProfilePic} alt="icon-profile" />
-          </ProfilePost>
+    return (
+        <>
+            <Post>
+                <div className="top">
+                    <ProfilePost>
+                        <img src={ProfilePic} alt="icon-profile" />
+                    </ProfilePost>
 
-          <div className="name">
-            <p>
-              {post.user.first_name} {post.user.last_name}
-            </p>
-            {/* <p>Avatar: {post.user.avatar}</p> */}
+                    <div className="name">
+                        <p>
+                            {post.user.first_name} {post.user.last_name}
+                        </p>
+                        {/* <p>Avatar: {post.user.avatar}</p> */}
 
-            <p>{Date.parse(post.created)}</p>
-            <p>{format(Date.parse(post.created), "MMMM dd")}</p>
-            <p>
-              {formatDistance(Date.parse(post.created), new Date(), {
-                addSuffix: true,
-              })}
-            </p>
-          </div>
+                        <p>{Date.parse(post.created)}</p>
+                        <p>{format(Date.parse(post.created), 'MMMM dd')}</p>
+                        <p>
+                            {formatDistance(
+                                Date.parse(post.created),
+                                new Date(),
+                                {
+                                    addSuffix: true,
+                                }
+                            )}
+                        </p>
+                    </div>
 
-          <p>
-            <input
-              type="checkbox"
-              id="following"
-              defaultChecked={post.logged_in_user_is_following}
-            />
-            <label for="liked">Is Following</label>
-          </p>
-          <p>
-            <input
-              type="checkbox"
-              id="friends"
-              defaultChecked={post.logged_in_user_is_friends}
-            />
-            <label for="friends">Is Friends</label>
-          </p>
+                    <p>
+                        <input
+                            type="checkbox"
+                            id="following"
+                            defaultChecked={post.logged_in_user_is_following}
+                        />
+                        <label for="liked">Is Following</label>
+                    </p>
+                    <p>
+                        <input
+                            type="checkbox"
+                            id="friends"
+                            defaultChecked={post.logged_in_user_is_friends}
+                        />
+                        <label for="friends">Is Friends</label>
+                    </p>
 
-          <MenuPost>
-            <img src={MenuIcon} alt="icon-profile" />
-          </MenuPost>
-        </div>
+                    <MenuPost>
+                        <img src={MenuIcon} alt="icon-profile" />
+                    </MenuPost>
+                </div>
 
-        <div className="post-content">
-          <p>{post.content}</p>
-        </div>
+                <div className="post-content">
+                    <p>{post.content}</p>
+                </div>
 
-        <div className="bottom">
-          <div className="Heart">
-            <img src={Heart} alt="heart" />
-            <p>Like</p>
-            <p>
-              <input
-                type="checkbox"
-                id="liked"
-                defaultChecked={post.logged_in_user_liked}
-              />
-              <label for="liked">Liked</label>
-            </p>
-          </div>
-          <div className="Share">
-            <img src={Share} alt="share" />
-            <p>Share</p>
-          </div>
-          <div className="Likes">
-            <p>{post.amount_of_likes} Likes</p>
-          </div>
-        </div>
-      </Post>
-    </>
-  );
+                <div className="bottom">
+                    <div className="Heart">
+                        <img src={Heart} alt="heart" />
+                        <p>Like</p>
+                        <p>
+                            <input
+                                type="checkbox"
+                                id="liked"
+                                defaultChecked={post.logged_in_user_liked}
+                            />
+                            <label for="liked">Liked</label>
+                        </p>
+                    </div>
+                    <div className="Share">
+                        <img src={Share} alt="share" />
+                        <p>Share</p>
+                    </div>
+                    <div className="Likes">
+                        <p>{post.amount_of_likes} Likes</p>
+                    </div>
+                </div>
+            </Post>
+        </>
+    );
 };
 
-export function PostsList() {
-  const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts.posts);
-  const postStatus = useSelector((state) => state.posts.status);
-  const error = useSelector((state) => state.posts.error);
-  const payload = { url: null, token: useSelector(selectUserToken) };
+export function PostsList(props) {
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.posts.posts);
+    const postStatus = useSelector((state) => state.posts.status);
+    const error = useSelector((state) => state.posts.error);
+    const payload = { url: null, token: useSelector(selectUserToken) };
+    console.log(props);
 
-  payload.url =
-    "https://motion.propulsion-home.ch/backend/api/social/posts/me/";
-  //   payload.url =
-  //     "https://motion.propulsion-home.ch/backend/api/social/posts/following/";
-  //   payload.url =
-  //     "https://motion.propulsion-home.ch/backend/api/social/posts/friends/";
-  //   payload.url =
-  //     "https://motion.propulsion-home.ch/backend/api/social/posts/likes/";
+    let url;
 
-  useEffect(() => {
-    if (postStatus === "idle") {
-      dispatch(fetchPosts(payload));
+    if (props.filter === 'currentUser') {
+        url = 'https://motion.propulsion-home.ch/backend/api/social/posts/me/';
+    } else {
+        url = 'https://motion.propulsion-home.ch/backend/api/social/posts/me/';
     }
-  }, [postStatus, dispatch, payload]);
 
-  let content;
+    payload.url = url;
 
-  if (postStatus === "succeeded") {
-    // Sort posts in reverse chronological order by created timestamp
-    const orderedPosts = posts
-      .slice()
-      .sort((a, b) => b.created.localeCompare(a.created));
+    //     'https://motion.propulsion-home.ch/backend/api/social/posts/me/';
+    //     "https://motion.propulsion-home.ch/backend/api/social/posts/following/";
+    //     "https://motion.propulsion-home.ch/backend/api/social/posts/friends/";
+    //     "https://motion.propulsion-home.ch/backend/api/social/posts/likes/";
 
-    console.log(orderedPosts);
+    useEffect(() => {
+        if (postStatus === 'idle') {
+            dispatch(fetchPosts(payload));
+        }
+    }, [postStatus, dispatch, payload]);
 
-    content = orderedPosts.map((post) => (
-      <PostExcerpt key={post.id} post={post} />
-    ));
-  } else if (postStatus === "failed") {
-    console.log("fail");
-    content = <div>{error}</div>;
-  }
+    let content;
 
-  
+    if (postStatus === 'succeeded') {
+        // Sort posts in reverse chronological order by created timestamp
+        const orderedPosts = posts
+            .slice()
+            .sort((a, b) => b.created.localeCompare(a.created));
 
-  return (
-    <PostsWrap>
-      <NewPost />
-      {content}
-    </PostsWrap>
-  );
+        console.log(orderedPosts);
+
+        content = orderedPosts.map((post) => (
+            <PostExcerpt key={post.id} post={post} />
+        ));
+    } else if (postStatus === 'failed') {
+        console.log('fail');
+        content = <div>{error}</div>;
+    }
+
+    return (
+        <PostsWrap>
+            <NewPost />
+            {content}
+        </PostsWrap>
+    );
 }
 
 export default PostsList;
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,77 +1,79 @@
 //Styled Components
 import {
-  StyledUserCard,
-  StyledDescription,
-  StyledName,
-} from "./StyledUserCard";
+    StyledUserCard,
+    StyledDescription,
+    StyledName,
+} from './StyledUserCard';
 import {
-  StyledHobbiesIcon,
-  StyledHobbies,
-} from "../styledComponents/StyledHobbies";
-import { SecondaryButton } from "../styledComponents/StyledButtons";
+    StyledHobbiesIcon,
+    StyledHobbies,
+} from '../styledComponents/StyledHobbies';
+import { SecondaryButton } from '../styledComponents/StyledButtons';
 
 //svg and imgs
-import DefaultImg from "../../assets/images/default.png";
+import DefaultImg from '../../assets/images/default.png';
 
 //libraries and hooks
-import axios from "axios";
-import { useSelector } from "react-redux";
-import { selectUserToken } from "../../store/slices/loginUser";
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { selectUserToken } from '../../store/slices/loginUser';
 
 function UserCard(props) {
-  const fullName = props.obj.first_name + " " + props.obj.last_name;
+    const fullName = props.obj.first_name + ' ' + props.obj.last_name;
 
-  const token = useSelector(selectUserToken);
+    const token = useSelector(selectUserToken);
 
-  const sendFriendRequest = () => {
-    const data = JSON.stringify({
-      status: "P",
-    });
+    const sendFriendRequest = () => {
+        const data = JSON.stringify({
+            status: 'P',
+        });
 
-    const config = {
-      method: "post",
-      url: `https://motion.propulsion-home.ch/backend/api/social/friends/request/${props.obj.id}/`,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      data: data,
+        const config = {
+            method: 'post',
+            url: `https://motion.propulsion-home.ch/backend/api/social/friends/request/${props.obj.id}/`,
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            data: data,
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
 
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+    return (
+        <StyledUserCard key={props.obj.id}>
+            {props.obj.avatar ? (
+                <img src={props.obj.avatar} alt="Avatar" />
+            ) : (
+                <img src={DefaultImg} alt="Avatar" />
+            )}
 
-  return (
-    <StyledUserCard key={props.obj.id}>
-      {props.obj.avatar ? (
-        <img src={props.obj.avatar} alt="Avatar" />
-      ) : (
-        <img src={DefaultImg} alt="Avatar" />
-      )}
-
-      <div>{props.id}</div>
-      <StyledName>{fullName}</StyledName>
-      <StyledDescription>{props.obj.location}</StyledDescription>
-      <div>
-        <SecondaryButton>FOLLOW</SecondaryButton>
-        <SecondaryButton onClick={sendFriendRequest}>
-          ADD FRIEND
-        </SecondaryButton>
-      </div>
-      <StyledDescription>{props.obj.about_me}</StyledDescription>
-      <StyledHobbies>
-        {props.obj.things_user_likes.map((item) => (
-          <StyledHobbiesIcon>{item}</StyledHobbiesIcon>
-        ))}
-      </StyledHobbies>
-    </StyledUserCard>
-  );
+            <div>{props.id}</div>
+            <StyledName>{fullName}</StyledName>
+            <StyledDescription>{props.obj.location}</StyledDescription>
+            <div>
+                <SecondaryButton>FOLLOW</SecondaryButton>
+                <SecondaryButton onClick={sendFriendRequest}>
+                    ADD FRIEND
+                </SecondaryButton>
+            </div>
+            <StyledDescription>{props.obj.about_me}</StyledDescription>
+            <StyledHobbies>
+                {props.obj.things_user_likes.map((item) => (
+                    <StyledHobbiesIcon>{item}</StyledHobbiesIcon>
+                ))}
+            </StyledHobbies>
+            {/* <p>is friends: {props.obj.logged_in_user_is_friends}</p>
+            <p>is friends: {props.obj.id}</p> */}
+        </StyledUserCard>
+    );
 }
 
 export { UserCard };
